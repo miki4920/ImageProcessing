03f23de0a400a8ea23e4c3e851c03766ce216177
Mikolaj Grobelny <miki4920@gmail.com>
2020-07-16 23:57:05 +0100

    Added Thresholding Function

--- image_processing.py
+++ image_processing.py
@@ -9,6 +9,30 @@ iar3 = np.array(i3)
 i4 = Image.open('images/sentdex.png')
 iar4 = np.array(i4)
 
+def threshold(imageArray):
+
+    balanceAr = []
+    newAr = imageArray
+    from statistics import mean
+    for eachRow in imageArray:
+        for eachPix in eachRow:
+            avgNum = mean(eachPix[:3])
+            balanceAr.append(avgNum)
+
+    balance = mean(balanceAr)
+    for eachRow in newAr:
+        for eachPix in eachRow:
+            if mean(eachPix[:3]) > balance:
+                eachPix[0] = 255
+                eachPix[1] = 255
+                eachPix[2] = 255
+                eachPix[3] = 255
+            else:
+                eachPix[0] = 0
+                eachPix[1] = 0
+                eachPix[2] = 0
+                eachPix[3] = 255
+    return newAr
 
 fig = plt.figure()
 ax1 = plt.subplot2grid((8,6),(0,0), rowspan=4, colspan=3)
